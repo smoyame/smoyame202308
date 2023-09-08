eca54cf8abb494b26ede9b4807707a8a3791c04d
smoyamedina <smoyamedina@outlook.com>
2023-09-08 12:35:35 -0400

    Adding tags to list

---
+++ backend/schemas/tags.ts
@@ -0,0 +1,13 @@
+// schemas/tags.ts
+export default {
+  name: 'tags',
+  title: 'Tags',
+  type: 'document',
+  fields: [
+    {
+      name: 'tag',
+      title: 'tag',
+      type: 'string',
+    },
+  ],
+}
