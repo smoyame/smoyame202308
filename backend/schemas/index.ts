e36086086777e9acac239c5ba68e5e263a02c1cd
smoyamedina <smoyamedina@outlook.com>
2023-09-08 12:36:50 -0400

    Apply new desk options

--- backend/schemas/index.ts
+++ backend/schemas/index.ts
@@ -1,3 +1,6 @@
 import projects from './projects'
+import selectWork from './selectWork'
+import about from './about'
+import tags from './tags'
 
-export const schemaTypes = [projects]
+export const schemaTypes = [projects, selectWork, about, tags]
