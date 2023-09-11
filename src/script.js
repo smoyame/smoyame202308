1e998c6414a4d5b539f570dcad33e61755797d40
smoyamedina <smoyamedina@outlook.com>
2023-09-11 15:04:37 -0400

    Init theme and flavor swapper

---
+++ src/script.js
@@ -0,0 +1,84 @@
+// Temp.  Typescript use later.
+const themeInput = document.querySelector("#toggle--theme");
+const smileySVG = document.querySelector(".footer-svg");
+
+const hasDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+const page = document.querySelector("[data-color-scheme]");
+const schemeAttribute = "data-color-scheme";
+const flavorAttribute = "data-color-flavor";
+
+let defaultScheme;
+let altScheme;
+let oppositeScheme;
+
+let flavor;
+let defaultFlavor;
+let scheme;
+
+/* -------------------------------------------------------------------------- */
+/*                                   Scheme                                   */
+/* -------------------------------------------------------------------------- */
+
+if (
+	(hasDarkMode.matches && sessionStorage.scheme == "undefined") ||
+	sessionStorage.scheme == "dark"
+) {
+	defaultScheme = "dark";
+	altScheme = "light";
+} else if (
+	(!hasDarkMode.matches && sessionStorage.scheme == "undefined") ||
+	sessionStorage.scheme == "light"
+) {
+	defaultScheme = "light";
+	altScheme = "dark";
+} else if (sessionStorage.scheme || sessionStorage.scheme !== "undefined") {
+	defaultScheme = sessionStorage.scheme;
+	altScheme = sessionStorage.oppositeScheme;
+}
+
+page.setAttribute(schemeAttribute, defaultScheme);
+
+const swapScheme = () => {
+	if (page.getAttribute(schemeAttribute) == defaultScheme) {
+		oppositeScheme = altScheme;
+	} else {
+		oppositeScheme = defaultScheme;
+	}
+
+	page.setAttribute(schemeAttribute, oppositeScheme);
+	sessionStorage.oppositeScheme = sessionStorage.scheme;
+	sessionStorage.scheme = oppositeScheme;
+};
+
+themeInput.addEventListener("change", swapScheme);
+
+/* -------------------------------------------------------------------------- */
+/*                                   Flavor                                   */
+/* -------------------------------------------------------------------------- */
+let altFlavors = ["alertness", "vibes", "studious", "confident", "default"];
+let place;
+
+if (sessionStorage.flavor !== "undefined" || sessionStorage.flavor == "default") {
+	defaultFlavor = sessionStorage.flavor;
+	place = (altFlavors.indexOf(sessionStorage.flavor) + 1) % altFlavors.length;
+} else {
+	defaultFlavor = "default";
+	place = 0;
+}
+
+page.setAttribute(flavorAttribute, defaultFlavor);
+
+smileySVG.addEventListener("click", () => {
+	flavor = altFlavors[place];
+
+	page.setAttribute(flavorAttribute, flavor);
+	sessionStorage.setItem("flavor", flavor);
+
+	place = (place + 1) % altFlavors.length;
+});
+
+// will be adding different schemes in the future
+
+/* -------------------------------------------------------------------------- */
+/*                                   Storage                                  */
+/* -------------------------------------------------------------------------- */
