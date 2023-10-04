e1e25c5c8a1f30783e69c52a58e05fb488606a1f
smoyamedina <smoyamedina@outlook.com>
2023-10-04 03:45:01 -0400

    Fetch filtered experiment pieces' info from api

---
+++ src/_fetch/experiments.ts
@@ -0,0 +1,13 @@
+export = async () => {
+	let response = await fetch(
+		"https://vzl61mfy.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22experiments%22%5D.gallery%5B%5D%7B%0A++title%2C%0A++medium%2C%0A++description%2C%0A++image%7B%0A++++alt%2C%0A++++%22URL%22%3A+asset-%3Eurl%0A++%7D+%0A%7D"
+	);
+
+	if (!response.ok) {
+		throw new Error(response.statusText);
+	}
+
+	let json = await response.json();
+
+	return json.result;
+};
