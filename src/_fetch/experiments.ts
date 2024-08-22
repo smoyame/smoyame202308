9f1a35efa9fa28ec554a00ca23f1c6c615688066
smoyamedina <smoyamedina@outlook.com>
2024-08-22 15:48:37 -0400

    Adding image metadata to data to help with lazy loading images.

--- src/_fetch/experiments.ts
+++ src/_fetch/experiments.ts
@@ -1,6 +1,6 @@
 export = async () => {
 	let response = await fetch(
-		"https://vzl61mfy.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22experiments%22%5D.gallery%5B%5D%7B%0A++title%2C%0A++medium%2C%0A++description%2C%0A++image%7B%0A++++alt%2C%0A++++%22URL%22%3A+asset-%3Eurl%0A++%7D+%0A%7D"
+		"https://vzl61mfy.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22experiments%22%5D.gallery%5B%5D%7B%0A++title%2C%0A++medium%2C%0A++description%2C%0A++image%7B%0A++++alt%2C%0A++++%22URL%22%3A+asset-%3Eurl%2C%0A++++%22dataImg%22%3A+asset-%3Emetadata.lqip%2C%0A++++%22blurHash%22%3A+asset-%3Emetadata.blurHash%2C%0A++++%22dimensions%22%3A+asset-%3Emetadata.dimensions%0A++%7D+%0A%7D"
 	);
 
 	if (!response.ok) {
